a0129fe71dfd7ea9319a4d22952671e952429d0c
Hope Giometti <hopegiometti@gmail.com>
2019-09-19 18:04:04 +0000

    Done.

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,3 +1,20 @@
+#def square_array(array)
+  #squared_array = []
+  #array.each { |i| squared_array << i ** 2 }
+  #squared_array
+#end
+
+#numbers = [1,2,3]
+#p square_array(numbers)
+
+
 def square_array(array)
-  # your code here
+  counter = 0
+
+  while counter < array.length do
+    array[counter] ** 2
+    counter +=1
+  end
 end
+
+square_array(numbers)
