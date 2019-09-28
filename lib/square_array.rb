514a8c53076432eda03f5a5b1f39ac11807f82c4
Muna Abdelrazeq <mma207@scarletmail.rutgers.edu>
2019-09-28 20:07:08 +0000

    Done.

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,3 +1,11 @@
 def square_array(array)
-  # your code here
+  i = 0
+  numbers = []
+  
+  while i < array.length do
+    number = array[i] ** 2
+    numbers.push(number)
+    i += 1
+  end 
+return numbers 
 end
